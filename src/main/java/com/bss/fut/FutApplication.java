a4f6f3708d993689f33b92c7c39446350aed2d4c
Louis Bedetti <louis.bedetti@etu.unice.fr>
2026-02-16 14:47:51 +0100

    initial commit

---
+++ src/main/java/com/bss/fut/FutApplication.java
@@ -0,0 +1,13 @@
+package com.bss.fut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bss/fut/FutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bss.fut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
